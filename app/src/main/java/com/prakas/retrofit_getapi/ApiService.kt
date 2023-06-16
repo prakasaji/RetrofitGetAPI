47630acf87a2c3e41f1f6472fa06170038ecf5fb
Prakas Aji Pratama <prakasaji.pratama@gmail.com>
2023-06-16 17:00:50 +0700

    Initial commit

---
+++ app/src/main/java/com/prakas/retrofit_getapi/ApiService.kt
@@ -0,0 +1,9 @@
+package com.prakas.retrofit_getapi
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("datamahasiswa/")
+    fun getdatamahasiswa(): Call<ApiResponse>
+}
